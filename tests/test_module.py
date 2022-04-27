a6777a319b362c26ca20616ba90d5ad7610a8d96
Chaz Schlarp <schlarpc@gmail.com>
2022-04-26 20:03:20 -0700

    Allow unit tests to work without hardcoded module name

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,25 +1,29 @@
-import json
 import runpy
 
 import pytest
 
-MODULE_NAME = "python_base_flake"
+
+@pytest.fixture
+def module_name(pytestconfig):
+    modules = [entry for entry in (pytestconfig.rootpath / "src").iterdir() if entry.is_dir()]
+    assert len(modules) == 1
+    return modules[0].name
 
 
-def test_import_works():
-    assert __import__(MODULE_NAME).__version__
+def test_import_works(module_name):
+    assert __import__(module_name).__version__
 
 
-def test_cli_runs_when_main(capsys):
+def test_cli_runs_when_main(module_name, capsys):
     try:
-        runpy.run_module(MODULE_NAME, run_name="__main__")
+        runpy.run_module(module_name, run_name="__main__")
     except SystemExit:
         pass
     captured = capsys.readouterr()
     assert captured.out or captured.err
 
 
-def test_cli_skipped_when_not_main(capsys):
-    runpy.run_module(MODULE_NAME)
+def test_cli_skipped_when_not_main(module_name, capsys):
+    runpy.run_module(module_name)
     captured = capsys.readouterr()
     assert not (captured.out or captured.err)
