b7ed3eee491aca4b2c13e85fd5bd72ae590cb961
Chaz Schlarp <schlarpc@gmail.com>
2022-05-29 16:48:48 -0700

    Commit all files in post-generate hook

--- .template/hooks/post_gen_project.py
+++ .template/hooks/post_gen_project.py
@@ -3,6 +3,8 @@ import subprocess
 
 def main():
     subprocess.run(["git", "init"], check=True)
+    subprocess.run(["git", "add", "."], check=True)
+    subprocess.run(["git", "commit", "-m", "Initial commit from template"], check=True)
 
 
 if __name__ == "__main__":
