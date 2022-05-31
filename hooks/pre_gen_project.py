14813a44397ad5be647de47e0781ca4810d78679
schlarpc <schlarpc@gmail.com>
2022-05-31 16:46:27 +0000

    Update template to output generated at 98df45f

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,17 +1,22 @@
+import json
 import re
 import sys
 
 REGEX_CHECKS = {
     "project_name": {
-        "value": "{{ cookiecutter.project_name }}",
+        "value": r""" {{ cookiecutter.project_name | jsonify }} """,
         "pattern": r"^[A-Za-z0-9-]+$",
+    },
+    "module_name": {
+        "value": r""" {{ cookiecutter.module_name | jsonify }} """,
+        "pattern": r"^[A-Za-z_][A-Za-z0-9_]+$",
     },
 }
 
 
 def main():
     for check_name, check in REGEX_CHECKS.items():
-        if not re.match(check["pattern"], check["value"]):
+        if not re.match(check["pattern"], json.loads(check["value"])):
             print(
                 f"ERROR: {check['value']!r} is not a valid {check_name}, "
                 f"must match {check['pattern']!r}",
