2d050a5e2fb9b7c4c9994c3dca048d2b5e8a554f
Chaz Schlarp <schlarpc@gmail.com>
2025-06-01 10:05:07 +0000

    Make template generation a bit safer

--- .github/workflows/generate-template.sh
+++ .github/workflows/generate-template.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
-PROJECT_NAME_PATTERN='s/python-base-flake/{{ cookiecutter.project_name }}/g'
-MODULE_NAME_PATTERN='s/python_base_flake/{{ cookiecutter.module_name }}/g'
-DEST=".template/{{ cookiecutter.project_name }}"
+PROJECT_NAME_PATTERN='s/python-base-flake/{{cookiecutter.project_name}}/g'
+MODULE_NAME_PATTERN='s/python_base_flake/{{cookiecutter.module_name}}/g'
+REPO="$(git rev-parse --show-toplevel)"
+DEST="$REPO/.template/{{cookiecutter.project_name}}"
 mkdir -p "$DEST"
+git clean -fXd -- "$DEST"
 
 if [[ -n "${GITHUB_REF-}" ]]; then
     # In Actions: strip refs/heads/ or refs/tags/ → branch|tag name
@@ -16,8 +18,11 @@ else
 fi
 while IFS= read -r -d '' entry; do
     NEW_NAME="$(sed "$MODULE_NAME_PATTERN;$PROJECT_NAME_PATTERN" <<< "$entry")"
-    mkdir -p "$(dirname "$DEST/$NEW_NAME")"
-    cp -pn -- "$entry" "$DEST/$NEW_NAME"
-    sed -i "$MODULE_NAME_PATTERN;$PROJECT_NAME_PATTERN" "$DEST/$NEW_NAME"
+    ENTRY_DEST="$DEST/$NEW_NAME"
+    mkdir -p "$(dirname "$ENTRY_DEST")"
+    if [ ! -e "$ENTRY_DEST" ]; then
+        cp -pn -- "$entry" "$ENTRY_DEST"
+        sed -i "$MODULE_NAME_PATTERN;$PROJECT_NAME_PATTERN" "$DEST/$NEW_NAME"
+    fi
 done < <("${FILE_CMD[@]}")
 rm -rf -- "$DEST/.template" "$DEST/.github/workflows/generate-template."{yml,sh}
